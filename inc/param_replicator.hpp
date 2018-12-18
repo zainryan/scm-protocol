3fb59a215c61f65eb023b954893189d9c77a5b89
Zhenyuan Ruan <zainryan95@gmail.com>
2018-12-18 21:26:40 +0000

    Fix code style.

--- inc/param_replicator.hpp
+++ inc/param_replicator.hpp
@@ -11,8 +11,7 @@ void param_replicator_impl(ST_Queue<unsigned int> *throttle_ratio_queue,
                            ST_Queue<unsigned int> *delay_cycles_0_queue,
                            ST_Queue<unsigned int> *delay_cycles_1_queue,
                            ST_Queue<unsigned int> *delay_cycles_2_queue,
-                           ST_Queue<unsigned int> *delay_cycles_3_queue
-                           );
+                           ST_Queue<unsigned int> *delay_cycles_3_queue);
 
 void param_replicator(ST_Queue<unsigned int> *throttle_ratio_queue,
                       ST_Queue<unsigned int> *throttle_ratio_0_queue,
