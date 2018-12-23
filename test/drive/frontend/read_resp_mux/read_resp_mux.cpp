db410e109e1c0ffe876061581bb87beead522c04
hetong07 <hetong07@gmail.com>
2018-12-23 20:13:21 +0000

    add the random test

--- test/drive/frontend/read_resp_mux/read_resp_mux.cpp
+++ test/drive/frontend/read_resp_mux/read_resp_mux.cpp
@@ -97,14 +97,13 @@ TEST(test_read_resp_mux, bank_1_resemble) {
 
 TEST(test_read_resp_mux, bank_2_resemble) {
   Read_Resp_Mux_Context context;
-  std::vector<std::vector<Chip_Read_Resp>>
-  input_read_resps(NUM_OF_CHIP_BANKS);
+  std::vector<std::vector<Chip_Read_Resp>> input_read_resps(NUM_OF_CHIP_BANKS);
   std::vector<std::vector<Chip_Read_Req>> input_read_reqs(NUM_OF_CHIP_BANKS);
   std::vector<Chip_Read_Resp_With_Addr> output_read_resp_with_addrs;
 
   for (int i = 0; i < TEST_NUM_OF_COMMANDS; i++) {
     Chip_Read_Resp chip_read_resp = RandomGen::rand_chip_read_resp();
-    Chip_Read_Req chip_read_req = RandomGen::rand_chip_read_req(2);
+    Chip_Read_Req chip_read_req = RandomGen::rand_chip_read_req(2 /*bank_id*/);
     input_read_resps[2].push_back(chip_read_resp);
     input_read_reqs[2].push_back(chip_read_req);
     context.chip_read_resp_queue[2]->write(chip_read_resp);
@@ -118,17 +117,14 @@ TEST(test_read_resp_mux, bank_2_resemble) {
   ASSERT_EQ(input_read_resps[2].size(), output_read_resp_with_addrs.size());
 
   for (int i = 0; i < output_read_resp_with_addrs.size(); i++) {
-    EXPECT_EQ(input_read_resps[2][i].data,
-    output_read_resp_with_addrs[i].data);
-    EXPECT_EQ(input_read_reqs[2][i].addr,
-    output_read_resp_with_addrs[i].addr);
+    EXPECT_EQ(input_read_resps[2][i].data, output_read_resp_with_addrs[i].data);
+    EXPECT_EQ(input_read_reqs[2][i].addr, output_read_resp_with_addrs[i].addr);
   }
 }
 
 TEST(test_read_resp_mux, bank_3_resemble) {
   Read_Resp_Mux_Context context;
-  std::vector<std::vector<Chip_Read_Resp>>
-  input_read_resps(NUM_OF_CHIP_BANKS);
+  std::vector<std::vector<Chip_Read_Resp>> input_read_resps(NUM_OF_CHIP_BANKS);
   std::vector<std::vector<Chip_Read_Req>> input_read_reqs(NUM_OF_CHIP_BANKS);
   std::vector<Chip_Read_Resp_With_Addr> output_read_resp_with_addrs;
 
@@ -148,9 +144,46 @@ TEST(test_read_resp_mux, bank_3_resemble) {
   ASSERT_EQ(input_read_resps[3].size(), output_read_resp_with_addrs.size());
 
   for (int i = 0; i < output_read_resp_with_addrs.size(); i++) {
-    EXPECT_EQ(input_read_resps[3][i].data,
-    output_read_resp_with_addrs[i].data);
-    EXPECT_EQ(input_read_reqs[3][i].addr,
-    output_read_resp_with_addrs[i].addr);
+    EXPECT_EQ(input_read_resps[3][i].data, output_read_resp_with_addrs[i].data);
+    EXPECT_EQ(input_read_reqs[3][i].addr, output_read_resp_with_addrs[i].addr);
+  }
+}
+
+TEST(test_read_resp_mux, all_bank_resemble) {
+  Read_Resp_Mux_Context context;
+  std::vector<std::vector<Chip_Read_Resp>> input_read_resps(NUM_OF_CHIP_BANKS);
+  std::vector<std::vector<Chip_Read_Req>> input_read_reqs(NUM_OF_CHIP_BANKS);
+  std::vector<Chip_Read_Resp_With_Addr> output_read_resp_with_addrs;
+
+  for (int i = 0; i < TEST_NUM_OF_COMMANDS; i++) {
+    Chip_Read_Resp chip_read_resp = RandomGen::rand_chip_read_resp();
+    Chip_Read_Req chip_read_req = RandomGen::rand_chip_read_req();
+    unsigned char bank_id =
+        (unsigned char)chip_read_req.addr & (NUM_OF_CHIP_BANKS - 1);
+    input_read_reqs[bank_id].push_back(chip_read_req);
+    input_read_resps[bank_id].push_back(chip_read_resp);
+    context.chip_read_resp_queue[bank_id]->write(chip_read_resp);
+    context.chip_read_req_context[bank_id]->write(chip_read_req);
+  }
+
+  RUN_METHOD(NUM_OF_CHIP_BANKS * TEST_NUM_OF_COMMANDS,
+             INVOKE_READ_RESP_MUX_IMPL(context));
+
+  drain_queue(context.chip_read_resp_with_addr_queue.get(),
+              &output_read_resp_with_addrs);
+
+  ASSERT_EQ(input_read_resps[0].size() + input_read_resps[1].size() +
+                input_read_resps[2].size() + input_read_resps[3].size(),
+            output_read_resp_with_addrs.size());
+
+  unsigned int bank_data_ptr[NUM_OF_CHIP_BANKS] = {0};
+  for (int i = 0; i < output_read_resp_with_addrs.size(); i++) {
+    unsigned char bank_id = (unsigned char)output_read_resp_with_addrs[i].addr &
+                            (NUM_OF_CHIP_BANKS - 1);
+
+    EXPECT_EQ(input_read_resps[bank_id][bank_data_ptr[bank_id]].data,
+              output_read_resp_with_addrs[i].data);
+    EXPECT_EQ(input_read_reqs[bank_id][bank_data_ptr[bank_id]++].addr,
+              output_read_resp_with_addrs[i].addr);
   }
 }
